c7d14ba77bb48bd2de7c75ebdaa881ed82555bf7
Wendeley <wendeleyf@gmail.com>
2021-01-02 14:00:43 -0300

    Callback com maps

---
+++ funcoes/callback_3.js
@@ -0,0 +1,22 @@
+const nums = [1,2,3,4,5]
+const dobro = (n, i, a) => n * 2 + i + a.length;
+console.log(nums.map(dobro));
+
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
+const primeiraLetra = texto => texto[0]
+console.log(nomes.map(primeiraLetra))
+
+const carrinho = [
+    {nome: 'Caneta', qtde: 10, preco: 7.99},
+    {nome: 'Impressora', qtde: 0, preco: 649.50},
+    {nome: 'Caderno', qtde: 4, preco: 27.10},
+    {nome: 'Lapis', qtde: 3, preco: 5.82},
+    {nome: 'Tesoura', qtde: 1, preco: 19.20},
+]
+
+const getNome = item => item.nome
+console.log(carrinho.map(getNome))
+
+const calcularPreco = item => item.qtde * item.preco
+const totais = carrinho.map(calcularPreco)
+console.log(totais)
